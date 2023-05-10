d249a0fccdc9f35f49e056ca07b4cb55dbe81f24
Carl Mercier <carl@carlmercier.com>
2023-05-09 22:40:18 -0400

    Don't crash when running migrations

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,11 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-# Load seeds on bootup
-unless Post.any?
-  puts "Seeding database..."
-  Rails.application.load_seed
+begin
+  # Load seeds on bootup
+  unless Post.any?
+    puts "Seeding database..."
+    Rails.application.load_seed
+  end
+rescue ActiveRecord::StatementInvalid
 end
