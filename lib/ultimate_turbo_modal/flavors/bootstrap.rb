056b250219ddb783a0dc254e9b970a90c6ea730c
Christian Finck <christian@finck.at>
2023-12-08 06:25:03 +0100

    added basic transitions

--- lib/ultimate_turbo_modal/flavors/bootstrap.rb
+++ lib/ultimate_turbo_modal/flavors/bootstrap.rb
@@ -3,8 +3,8 @@
 module UltimateTurboModal::Flavors
   class Bootstrap < UltimateTurboModal::Base
     DIV_DIALOG_CLASSES = "modal-container"
-    DIV_OVERLAY_CLASSES = "modal-backdrop fade show"
-    DIV_OUTER_CLASSES = "modal fade show d-block"
+    DIV_OVERLAY_CLASSES = "modal-backdrop opacity-50"
+    DIV_OUTER_CLASSES = "modal d-block"
     DIV_INNER_CLASSES = "modal-dialog"
     DIV_CONTENT_CLASSES = "modal-content"
     DIV_MAIN_CLASSES = "modal-body"
@@ -13,6 +13,35 @@ module UltimateTurboModal::Flavors
     DIV_TITLE_H_CLASSES = "modal-title-h"
     DIV_FOOTER_CLASSES = "modal-footer d-block"
     CLOSE_BUTTON_TAG_CLASSES = "btn-close"
+
+    def div_dialog(&)
+      div(id:    "modal-container",
+          class: self.class::DIV_DIALOG_CLASSES,
+          role:  "dialog",
+          aria:  {
+            labeled_by: "modal-title-h",
+            modal: true
+          },
+          data:  {
+            controller: "modal",
+            modal_target: "container",
+            modal_advance_url_value: advance_url,
+            modal_allowed_click_outside_selector_value: allowed_click_outside_selector,
+            action: "turbo:submit-end->modal#submitEnd keyup@window->modal#closeWithKeyboard click@window->modal#outsideModalClicked click->modal#outsideModalClicked",
+            transition_enter: "fade",
+            transition_enter_start: "",
+            transition_enter_end: "show",
+            transition_leave: "fade",
+            transition_leave_start: "show",
+            transition_leave_end: "",
+            padding: padding?.to_s,
+            title: title?.to_s,
+            header: header?.to_s,
+            close_button: close_button?.to_s,
+            header_divider: header_divider?.to_s,
+            footer_divider: footer_divider?.to_s
+          }, &)
+    end
 
     def div_title
       if @title_block.present?
